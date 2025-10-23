681ea9508c02967b578717e25e939f644381150b
José Daniel Grayson <josedanielgrayson@proton.me>
2025-10-22 22:48:41 -0500

    style: separate function declarations in wavelet.h

--- include/wavelet.h
+++ include/wavelet.h
@@ -7,10 +7,13 @@
 typedef struct WaveletTree WaveletTree;
 
 WaveletTree* wavelet_from_vec(const u32* restrict vals, size_t n);
+
 WaveletTree* wavelet_from_string(const char* restrict str);
+
 void wavelet_destroy(WaveletTree* wavelet);
 
 u32 wavelet_kth(const WaveletTree* wavelet, u32 l, u32 r, u32 k);
+
 u32 wavelet_leq(const WaveletTree* wavelet, u32 l, u32 r, u32 k);
 
 #endif
